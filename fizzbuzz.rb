cd8859a87a8ad0bb41b65eedc503c7d64c568de2
RonakAgarwal <agarwalronak02@gmail.com>
2017-12-19 22:34:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(number) {
-  return "FizzBuzz" if divisible_by_15?(number)
-  return "Fizz" if divisible_by_3?(number)
-  return "Buzz" if divisible_by_5?(number)
+  return "FizzBuzz" if divisible_by_fifteen?(number)
+  return "Fizz" if divisible_by_three?(number)
+  return "Buzz" if divisible_by_five?(number)
 }
